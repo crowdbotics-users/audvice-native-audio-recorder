aa754ccaccaf53ba6536f30cd3ce9840b5bd27bd
scbopha <bopha@crowdbotics.com>
2019-01-26 09:42:15 +0700

    move code to main library

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
+import React from 'react'
+import {
+  NativeModules,
+  Platform,
+  Text,
+  StyleSheet,
+  ViewPropTypes,
+  requireNativeComponent,
+  UIManager,
+  findNodeHandle
+} from 'react-native'
+import PropTypes from 'prop-types'
 
-import { NativeModules } from 'react-native';
+const { RNAudioRecorder } = NativeModules
+const RNAudioRecorderView = requireNativeComponent('RNAudioRecorderView')
 
-const { RNAudioRecorder } = NativeModules;
+export default class AudioRecorder extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-export default RNAudioRecorder;
+  initialize(filename, startTimeInMS) {
+    RNAudioRecorder.initialize(findNodeHandle(this.recorderView), filename, startTimeInMS)
+  }
+
+  renderByFile(filename) {
+    return RNAudioRecorder.renderByFile(findNodeHandle(this.recorderView), filename)
+  }
+
+  startRecording() {
+    RNAudioRecorder.startRecording(findNodeHandle(this.recorderView))
+  }
+
+  stopRecording(){
+    return RNAudioRecorder.stopRecording(findNodeHandle(this.recorderView))      
+  }
+
+  play() {
+    RNAudioRecorder.play(findNodeHandle(this.recorderView))
+  }
+
+  cut(filename, fromTime, toTime){
+    return RNAudioRecorder.cut(findNodeHandle(this.recorderView), filename, fromTime, toTime)
+  }
+
+  destroy() {
+    return RNAudioRecorder.destroy(findNodeHandle(this.recorderView))
+  }
+
+  render() {
+    const {
+      width,
+      height,
+      onScroll,
+      pixelsPerSecond,
+      plotLineColor,
+      timeTextColor,
+      timeTextSize
+    } = this.props
+    return(
+      <RNAudioRecorderView style={this.props.style}
+        ref={ref => this.recorderView = ref}
+        onScroll={onScroll}
+        pixelsPerSecond={pixelsPerSecond}
+        plotLineColor={plotLineColor}
+        timeTextColor={timeTextColor}
+        timeTextSize={timeTextSize} />
+    )
+  }
+}
+
+AudioRecorder.propTypes = {
+  ...ViewPropTypes,
+  onScroll: PropTypes.bool,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  pixelsPerSecond: PropTypes.number,
+  plotLineColor: PropTypes.string,
+  timeTextColor: PropTypes.string,
+  timeTextSize: PropTypes.number
+}
+
+AudioRecorder.defaultProps = {
+  onScroll: true,
+  height: 0,
+  width: 0,
+  pixelsPerSecond: 50,
+  plotLineColor: 'white',
+  timeTextColor: 'white',
+  timeTextSize: 20
+}
